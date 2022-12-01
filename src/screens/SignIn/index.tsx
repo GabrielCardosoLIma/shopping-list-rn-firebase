bbdde0980890d59da8d7871feac8ffdce2100f09
GABRIEL CARDOSO LIMA <gabriel.clima15@senacsp.edu.br>
2022-11-30 22:26:30 -0300

    update 30.11.2022

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container, Account, Title, Subtitle } from './styles';
 import { ButtonText } from '../../components/ButtonText';
@@ -6,24 +6,34 @@ import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import auth from '@react-native-firebase/auth';
 import { useNavigation } from '@react-navigation/native';
+import { Alert } from 'react-native';
 
 export function SignIn() {
+
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
 
   const navigation = useNavigation();
 
   function handleSignIn(){
     auth()
-    .signInWithEmailAndPassword('limag656@gmail.com', '123456')
+    .signInWithEmailAndPassword((user), (password))
     .then(() => {
       navigation.navigate("Produtos");
     })
     .catch(error => {
       if (error.code === 'auth/email-already-in-use') {
-        console.log('That email address is already in use!');
+        Alert.alert('That email address is already in use!');
       }
   
       if (error.code === 'auth/invalid-email') {
-        console.log('That email address is invalid!');
+        Alert.alert('That email address is invalid!');
+      }
+      if (error.code === 'auth/user-not-found') {
+        Alert.alert('That email address is invalid!');
+      }
+      if (error.code === 'auth/wrong-password') {
+        Alert.alert('Password is invalid!');
       }
       console.error(error);
     });
@@ -37,11 +47,13 @@ export function SignIn() {
       <Input
         placeholder="e-mail"
         keyboardType="email-address"
+        onChangeText={setUser}
       />
 
       <Input
         placeholder="senha"
         secureTextEntry
+        onChangeText={setPassword}
       />
 
       <Button title="Entrar" onPress={handleSignIn} />
